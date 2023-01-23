d2ce1fca7406301a9619e1e7cd5094dde13c89a3
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-01-23 12:41:20 +0200

    add LS Btn (#20)

--- src/js/modal-film-info.js
+++ src/js/modal-film-info.js
@@ -5,14 +5,20 @@ const refs = {
     backdropEl: document.querySelector('.js-backdrop'),
     closeModalEl: document.querySelector('.js-modal-close'),
     infoCard:document.querySelector('.description-modal_info'),
+    watchedBtn:document.querySelector('.js-watched'),
+    queueBtn:document.querySelector('.js-queue'),
 };
 
+const movieAPI = new MovieAPI();
+
 const createCards = cardInfo => {
-    const { poster_path, title, vote_average, vote_count, popularity, original_title, genres, overview } = cardInfo
+    const { poster_path, title, vote_average, vote_count, popularity, original_title, genres, overview,id } = cardInfo
     const genresEl = [];
     for (genre of genres) {
-        genresEl.push(genre.name);
-}
+        genresEl.push(genre.name);        
+    }
+    refs.watchedBtn.dataset.filmId = id;
+    refs.queueBtn.dataset.filmId = id;
     return `
                <img
         class="description-modal_img"
@@ -43,11 +49,7 @@ const createCards = cardInfo => {
             <p class="description-modal_about">ABOUT</p>
             <p class="description-modal_about">${overview}</p>
           </li>          
-        </ul>
-        <div class="description-btn">
-        <button class="description-modal_btn watched">add to Watched</button>
-        <button class="description-modal_btn queue">add to queue</button>
-        </div>
+        </ul>       
     </div>        
             `;    
 };
@@ -55,30 +57,78 @@ const createCards = cardInfo => {
 const addMoveInfo = async (id) => {
     try {
         const data = await movieAPI.getFilmFullInfo(id);
-        //console.log(data);        
-        refs.infoCard.innerHTML = createCards(data);  
-                
+        //console.log('61',data);        
+        refs.infoCard.innerHTML = createCards(data);                  
     } catch (err) {
         console.log(err);
     }
 };
 
-const movieAPI = new MovieAPI();
+const saveToLS = (key, value) => {
+  try {
+    const val = JSON.stringify(value);
+    localStorage.setItem(key, val);
+  } catch (error) {
+    console.error("Set state error: ", error.message);
+  }
+};
 
-const onModalOpen = e => {
+const loadToLS = key => {
+  try {
+      const serializedState = localStorage.getItem(key);
+      return serializedState === null ? [] : JSON.parse(serializedState);
+    } catch (error) {
+    console.error("Set state error: ", error.message);
+  }
+};
+
+const onModalOpen =async e => {
     e.preventDefault();    
-    //console.log(e);    
+    //console.log('88',e.target.dataset);    
     if (e.target.offsetParent.className!=="card-list__item") {
         return;
-    }
+    } 
     const idFilm = e.target.dataset.id;
-    addMoveInfo(idFilm)
-    //console.log(movieAPI.getFilmFullInfo(idFilm));
+    //const data = await movieAPI.getFilmFullInfo(idFilm);
+    //console.log('96',data);
 
+    addMoveInfo(idFilm)    
     refs.backdropEl.classList.remove('is-hidden');
     document.body.classList.add('no-scroll');
-    document.addEventListener('keydown', onEscKeyPress);
+    document.addEventListener('keydown', onEscKeyPress);    
 };
+
+// localStorage.removeItem('filmWatched');
+// localStorage.removeItem('filmQueue');
+
+let arrFilmWatched=loadToLS('filmWatched');  
+let arrFilmQueue=loadToLS('filmQueue');
+
+//arrFilmWatched=loadToLS('filmWatched')
+console.log(arrFilmWatched);
+//arrFilmQueue=loadToLS('filmQueue')
+console.log(arrFilmQueue);
+
+const onBtnWatchedClick=e=>{
+  e.preventDefault();
+       
+  const idFilm=refs.watchedBtn.dataset.filmId
+  arrFilmWatched.push(idFilm)
+  const filterArrFilmWatched=arrFilmWatched.filter((value, i, arr)=>arr.indexOf(value)===i)
+  // console.log('122',filterArrFilmWatched);
+  // console.log('123',arrFilmWatched);
+  saveToLS('filmWatched', filterArrFilmWatched)
+ }
+
+const onBtnQueueClick= e=>{
+e.preventDefault();  
+const idFilm=refs.queueBtn.dataset.filmId
+arrFilmQueue.push(idFilm)
+const filterArrFilmQueue=arrFilmQueue.filter((value, i, arr)=>arr.indexOf(value)===i)    
+// console.log('135',filterArrFilmQueue);
+// console.log('136',arrFilmQueue);
+saveToLS('filmQueue', filterArrFilmQueue)   
+}
 
 const closeModal = () => {    
     refs.backdropEl.classList.add('is-hidden');
@@ -104,4 +154,30 @@ const onBackdropElClick = e => {
 refs.modalOpenEl.addEventListener('click', onModalOpen);
 refs.closeModalEl.addEventListener('click', closeModal);
 refs.backdropEl.addEventListener('click', onBackdropElClick);
+refs.watchedBtn.addEventListener('click', onBtnWatchedClick);
+refs.queueBtn.addEventListener('click', onBtnQueueClick);
 
+
+
+
+
+
+//     const saveToLS = (key, value) => {
+//       try {
+//         const val = JSON.stringify(value);
+//         localStorage.setItem(key, val);
+//       } catch (error) {
+//         console.error("Set state error: ", error.message);
+//       }
+//     };
+
+// const ArrFilmWatched = ["76600","653851","615777","36554"]
+// const ArrFilmQueue = ["996727","778946","674324","990140"]
+
+//     saveToLS('filmWatched', ArrFilmWatched)
+
+//     saveToLS('filmQueue', ArrFilmQueue)
+
+
+// localStorage.removeItem('filmWatched1');
+// localStorage.removeItem('filmQueue1');
