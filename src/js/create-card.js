7ff214faea475c0d384e222929a3a8536e1db6b3
iTommma <116196545+iTommma@users.noreply.github.com>
2023-01-24 00:08:24 +0200

    fixed display of gallery cards (#29)
    * fixed display of gallery cards
    
    * page 1

--- src/js/create-card.js
+++ src/js/create-card.js
@@ -52,12 +52,16 @@ export function createGallery(results) {
         return `
           <li class="card-list__item">
             <a href="#" class="card-list_link" id="${id}">
-              <img class="card-list__img" data-id="${id}" src="https://image.tmdb.org/t/p/w500${poster_path}" alt=" ${title} ">
-              <h3 class="card-list__title">${title}</h1>
-              <div class="card-list__info">
-                  <p class="card-list__text">${genre} |  ${year} </p>
-                  <div class="card-list__rate-box"><p class="card-list__rate">${average}</p></div>
+              <div class="card-list__img-box">
+                <img class="card-list__img" data-id="${id}" src="https://image.tmdb.org/t/p/w500${poster_path}" alt=" ${title} ">
               </div>
+                <h3 class="card-list__title">${title}</h1>
+                <div class="card-list__info">
+                  <p class="card-list__text">${genre} |  ${year} </p>
+                  <div class="card-list__rate-box">
+                    <p class="card-list__rate">${average}</p>
+                  </div> 
+                </div>
             </a>
           </li>`;
       }
@@ -65,8 +69,10 @@ export function createGallery(results) {
       return `
         <li class="card-list__item">
           <a href="#" class="card-list_link" id="${id}">
-            <img class="card-list__img" data-id="${id}" src="https://image.tmdb.org/t/p/w500${poster_path}" alt=" ${title} ">
-            <h3 class="card-list__title">${title}</h1>
+            <div class="card-list__img-box">
+              <img class="card-list__img" data-id="${id}" src="https://image.tmdb.org/t/p/w500${poster_path}" alt=" ${title} ">
+            </div>
+            <h3 class="card-list__title">${title}</h3>
             <p class="card-list__text">${genre} |  ${year} </p>
           </a>
         </li>`;
